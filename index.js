7fd66d535888bccf8ac6e4641e0d363b0f953938
Finn <bh2@qq.com>
2019-10-21 10:32:00 +0800

    添加缓存功能

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-﻿var _start = require('./assets/lib/plugin');
+var _start = require('./assets/lib/plugin');
 var Config = require('./assets/lib/config');
+var fs = require("fs");
 require('./assets/lib/log');
+
+var os = require('os');
+var path = require('path');
+
+
+// 缓存结果，加速
+// __doc__: {$path: {content: "编译后的内容", mtimeMs: "mtimeMs相同，表示不需要重新编译"}}
+var cached = {};
+var cache_file = path.join(os.tmpdir(), 'anchor-navigation-ex.cache.json');
+if(fs.existsSync(cache_file)){
+    cached = JSON.parse(fs.readFileSync(cache_file));
+}
+
+
 module.exports = {
     book: {
         assets: "./assets",
@@ -11,11 +26,27 @@ module.exports = {
             Config.handlerAll(this);
         },
         "page": function (page) {
-            if (Config.config.printLog) {
-                console.info("INFO:".info + "当前正在处理:" + page.path)
+            var file_stat = fs.statSync(page.rawPath);
+            var cached_stat = cached[page.rawPath] || {};
+
+            // 有缓存，直接返回缓存的结果
+            if(cached_stat.mtimeMs == file_stat.mtimeMs) {
+                page.content = cached_stat.content;
+                return page;
             }
+
+            if (Config.config.printLog) {
+                console.info("INFO:".info + "正在生成导航目录:" + page.path)
+            }
+
             var bookIns = this;
             _start(bookIns, page);
+
+            cached_stat.mtimeMs = file_stat.mtimeMs;
+            cached_stat.content = page.content;
+            cached[page.rawPath] = cached_stat;
+            fs.writeFileSync(cache_file, JSON.stringify(cached, null, 4));
+
             return page;
         }
     }
